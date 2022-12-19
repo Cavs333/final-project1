cbbe9778ce3a020937c8c1273673af72b8254ee6
Cavs333 <121043427+Cavs333@users.noreply.github.com>
2022-12-19 10:26:32 -0500

    Initial commit

---
+++ Dogs.cs
@@ -0,0 +1,38 @@
+﻿/* Program Name: Example
+Programmer: Logan Farlow
+ Date: 12 / 19 / 2022
+ Description: What does your program do? Be sure
+  to keep everything within view on your screen. Edit
+ this description to match what your program does. For
+  example, "This program allows you to  Decribe your dog Name age an Breed and color
+ users in a C# windows console application
+*/
+
+public class Dog
+
+
+{
+    // properties
+    public string Breed { get; set; }
+    public int Age { get; set; }
+    public string Color { get; set; }
+
+    // constructor
+    public Dog(string breed, int age, string color)
+    {
+        Breed = breed;
+        Age = age;
+        Color = color;
+    }
+
+    // methods
+    public void Bark()
+    {
+        Console.WriteLine("Woof! Woof!");
+    }
+
+    public void Fetch()
+    {
+        Console.WriteLine("Fetching the ball...");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DogExample
+{
+    // This is the Dog class. It has two properties: Name and Breed.
+    class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+
+        // This is the constructor for the Dog class. It takes two arguments:
+        // the name and breed of the dog.
+        public Dog(string name, string breed)
+        {
+            Name = name;
+            Breed = breed;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new List of Dogs.
+            List<Dog> dogs = new List<Dog>();
+
+            // Add some dogs to the list.
+            dogs.Add(new Dog("Buddy", "Golden Retriever"));
+            dogs.Add(new Dog("Daisy", "Beagle"));
+            dogs.Add(new Dog("Max", "German Shepherd"));
+
+            // Loop through the list of dogs and print their names and breeds.
+            foreach (Dog dog in dogs)
+            {
+                Console.WriteLine($"{dog.Name} is a {dog.Breed}.");
+            }
+        }
+    }
+}
